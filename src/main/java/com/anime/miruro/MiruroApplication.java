402eb8151f485f99b2d5a00c1c1564ac521e52be
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2025-01-23 12:21:26 +0100

    start

---
+++ src/main/java/com/anime/miruro/MiruroApplication.java
@@ -0,0 +1,13 @@
+package com.anime.miruro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiruroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiruroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anime/miruro/MiruroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.miruro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiruroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
