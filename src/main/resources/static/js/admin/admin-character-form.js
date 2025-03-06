732c357053f9f8d06cef45e20530ac27c2e0c87b
lorenzo-vlgs <enzo.villegas27@gmail.com>
2025-03-06 09:35:42 +0100

    aggiornato character e character-form

--- src/main/resources/static/js/admin/admin-character-form.js
+++ src/main/resources/static/js/admin/admin-character-form.js
@@ -2,33 +2,37 @@
 let idChar = "-1";
 let idAnime = "0";
 
-document.addEventListener('DOMContentLoaded', () => {
+const chaName = document.getElementById('chaName');
+const chaImage = document.getElementById('chaImage');
+const description = document.getElementById('description');
+const role = document.getElementById('role');
+const anime = document.getElementById('anime-space');
+const image = document.getElementById('imageDisplay');
+
+document.addEventListener('DOMContentLoaded', async () => {
     // Get the 'id' parameter from the URL
     const urlParams = new URLSearchParams(window.location.search);
-    // idChar = urlParams.get('idCharacter');
-    idAnime = urlParams.get('id');
 
-    if (urlParams.get('idChar')) {
-        idChar = urlParams.get('idChar');
-        console.log(" ID CHAR: " + idChar);
+    if ( urlParams.get('id')) idAnime = urlParams.get('id');
+    if (urlParams.get('idChar')) idChar = urlParams.get('idChar');
+
+    if (idChar > 0) {
+        const data = await httpService.invoke(`/api/characters/${idChar}`, 'GET');
+        if (data) {
+            chaName.value = data.name;
+            chaImage.value = data.image;
+            description.value = data.description;
+            role.value = data.role;
+            image.src = data.image;;
+        }
     }
 
-    if (idChar > 0) getCharacterById(idChar);
-    if (idAnime > 0) ;
 });
 
 
 //
-// Updates the image as soon as a URL is entered in the input field
+// API RELATED 
 //
-document.getElementById('chaImage').addEventListener('input', function() {
-
-    // Displays the image after entering the URL in the input field
-    const url = this.value;
-    document.getElementById('imageDisplay').src = url;
-});
-
-
 document.getElementById('character-form').addEventListener('submit', function(event) {
 
     event.preventDefault();
@@ -49,18 +53,29 @@ document.getElementById('character-form').addEventListener('submit', function(ev
         "animeId": idAnime
     };
 
-    // console.log(animeData);
-
     // Call the save/update call
     let url = idChar === "-1" ? '/api/characters/save' : '/api/characters/update';
 
     console.log(url);
-    postCharacter(url, animeData);
+    httpService.invoke(url, 'POST', JSON.stringify(animeData));
     
     // Show alert and refresh page after 3 seconds
     showAlertAndRefresh();
     
 });
+
+
+//
+// Updates the image as soon as a URL is entered in the input field
+//
+document.getElementById('chaImage').addEventListener('input', function() {
+
+    // Displays the image after entering the URL in the input field
+    const url = this.value;
+    document.getElementById('imageDisplay').src = url;
+});
+
+
 
 function showAlertAndRefresh() {
     const alertPlaceholder = document.getElementById('alert-placeholder');
