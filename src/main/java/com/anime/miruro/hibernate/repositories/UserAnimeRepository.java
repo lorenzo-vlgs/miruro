354934d5c470ec84dc2c4d804a6dd9b5015843f7
lorenzo-vlgs <enzo.villegas27@gmail.com>
2025-03-10 12:29:43 +0100

    added reposotories and services

---
+++ src/main/java/com/anime/miruro/hibernate/repositories/UserAnimeRepository.java
@@ -0,0 +1,10 @@
+package com.anime.miruro.hibernate.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anime.miruro.hibernate.entities.UserAnime;
+import com.anime.miruro.hibernate.entities.UserAnimeId;
+
+public interface UserAnimeRepository extends JpaRepository<UserAnime, UserAnimeId>{
+    
+}
---
+++ src/main/java/com/anime/miruro/hibernate/services/UserAnimeService.java
@@ -0,0 +1,10 @@
+package com.anime.miruro.hibernate.services;
+
+import com.anime.miruro.hibernate.entities.UserAnime;
+import com.anime.miruro.hibernate.entities.UserAnimeId;
+import com.anime.miruro.hibernate.repositories.UserAnimeRepository;
+import com.anime.miruro.services.GenericService;
+
+public class UserAnimeService extends GenericService<UserAnimeId, UserAnime, UserAnimeRepository>{
+    
+}
